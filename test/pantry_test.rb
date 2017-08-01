c3884825698397e3bdb0f589ceb114bc76c91081
Becki Srofe <blf_23@yahoo.com>
2017-08-01 09:09:15 -0600

    Add pantry initialize and test

--- test/pantry_test.rb
+++ test/pantry_test.rb
@@ -1,7 +1,13 @@
 require './lib/pantry'
 require 'minitest/autorun'
 require 'minitest/pride'
+require 'minitest/emoji'
 
 class PantryTest < Minitest::Test
+
+  def test_it_exists
+    pantry = Pantry.new
+    assert_instance_of Pantry, pantry
+  end
 
 end
