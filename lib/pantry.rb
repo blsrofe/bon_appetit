d0c90b0e0438c9cc6d81c37a256ac26a972e3cbb
Becki Srofe <blf_23@yahoo.com>
2017-08-01 09:14:31 -0600

    Add stock_check test and method

--- lib/pantry.rb
+++ lib/pantry.rb
@@ -1,9 +1,17 @@
 class Pantry
-  
+
   attr_reader :stock
 
   def initialize
     @stock = {}
+  end
+
+  def stock_check(ingredient)
+    if stock[ingredient.downcase].nil?
+      0
+    else
+      stock[ingredient.downcase]
+    end
   end
 
 end
--- test/pantry_test.rb
+++ test/pantry_test.rb
@@ -10,4 +10,13 @@ class PantryTest < Minitest::Test
     assert_instance_of Pantry, pantry
   end
 
+  def test_it_starts_empty
+    pantry = Pantry.new
+    assert_equal ({}), pantry.stock
+  end
+
+  def test_it_can_check_for_ingredients
+    pantry = Pantry.new
+    assert_equal 0, pantry.stock_check("Cheese")
+  end
 end
