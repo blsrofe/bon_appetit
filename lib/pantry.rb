8cf48c706922f819b7dca03204ea81764d003cec
Becki Srofe <blf_23@yahoo.com>
2017-08-01 09:39:34 -0600

    Add convert unit method, not working yet

--- lib/pantry.rb
+++ lib/pantry.rb
@@ -23,4 +23,27 @@ class Pantry
     end
   end
 
+  def convert_units(recipe)
+    recipe.ingredients.map do |ingredient, amount|
+      new_amount = transform(amount)
+      recipe.ingredients[ingredient] = new_amount
+    end
+    recipe.ingredients
+  end
+
+  def transform(amount)
+    transformed_collection = {}
+    if amount > 100
+      transformed_collection[:quantity] = amount * 100
+      transformed_collection[:units] = "Centi-Units"
+    elsif amount < 1
+      transformed_collection[:quantity] = amount
+      transformed_collection[:units] = "Milli-Units"
+    else
+      transformed_collection[:quantity] = amount / 1000
+      transformed_collection[:units] = "Universal Units"
+    end
+    transformed_collection
+  end
+
 end
